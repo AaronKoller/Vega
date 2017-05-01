ec48a9b9f34a2374ca2bfcf9f01e27de6756cb51
Aaron Koller <aaronkoller777@gmail.com>
2017-04-30 23:35:19 -0700

    Added dbContext to Persistance folder and the startup.cs

--- Models/Make.cs
+++ Models/Make.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace Vega.Models
 {
@@ -7,5 +8,10 @@ namespace Vega.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public ICollection<Model> Models { get; set; }
+
+        public Make()
+        {
+            Models = new Collection<Model>();
+        }
     }
 }
