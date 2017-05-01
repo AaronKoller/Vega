690cc93d89ddc3e17cf935ee14d2949fd331a725
AaronKoller <aaronkoller777@gmail.com>
2017-05-01 11:08:47 -0700

    Building the api/makes api

--- Controllers/MakesController.cs
+++ Controllers/MakesController.cs
@@ -1,7 +1,12 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Vega.Models;
+
 namespace Vega.Controllers
 {
-    public class MakesController
+    public class MakesController :  Controller
     {
-        
+        [HttpGet("/api/makes")]
+        public IEnumerable<Make> GetMakes()
     }
 }
