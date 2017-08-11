e9969272b0f1ad07a247e1cc43acd0cb079d0539
Aaron Koller <aaronkoller777@gmail.com>
2017-08-10 22:37:16 -0700

    Add a vehicle record to the database via API.

--- Controllers/VehiclesController.cs
+++ Controllers/VehiclesController.cs
@@ -1,24 +1,35 @@
+using System;
+using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Vega.Controllers.Resources;
 using Vega.Models;
+using Vega.Persistence;
 
 namespace Vega.Controllers
 {
-  [Route("/api/vehicles")]
-  public class VehiclesController : Controller
-  {
-    private readonly IMapper mapper;
-    public VehiclesController(IMapper mapper)
+    [Route("/api/vehicles")]
+    public class VehiclesController : Controller
     {
-        this.mapper = mapper;
-    }
+        private readonly IMapper mapper;
+        private readonly VegaDbContext context;
+        public VehiclesController(IMapper mapper, VegaDbContext context)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
 
-    [HttpPost]
-    public IActionResult CreateVehicle([FromBody] VehicleResource vehicleResource)
-    {
-        var vehicle = mapper.Map<VehicleResource, Vehicle>(vehicleResource);
-        return Ok(vehicle);
+        [HttpPost]
+        public async Task<IActionResult> CreateVehicle([FromBody] VehicleResource vehicleResource)
+        {
+            var vehicle = mapper.Map<VehicleResource, Vehicle>(vehicleResource);
+            vehicle.LastUpdate = DateTime.Now;
+            
+            context.Vehicles.Add(vehicle);
+            await context.SaveChangesAsync(); 
+
+            var result = mapper.Map<Vehicle, VehicleResource>(vehicle);
+            return Ok(result);
+        }
     }
-  }
 }
