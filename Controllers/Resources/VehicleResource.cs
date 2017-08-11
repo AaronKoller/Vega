94a807c5cf788de5815f51642b087f89eceefe63
Aaron Koller <aaronkoller777@gmail.com>
2017-08-10 23:14:35 -0700

    VehicleController Validation

--- Controllers/Resources/VehicleResource.cs
+++ Controllers/Resources/VehicleResource.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
 using Vega.Models;
 
 namespace Vega.Controllers.Resources
@@ -11,6 +12,7 @@ namespace Vega.Controllers.Resources
         public int Id { get; set; }
         public int ModelId { get; set; }
         public bool IsRegistered { get; set; }
+        [Required]
         public ContactResource Contact { get; set; }
         public ICollection<int> Features { get; set; }
         public VehicleResource()
