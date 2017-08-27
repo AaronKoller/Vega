602d192efd51cdf4c984716f3cce8b6e25c4dbb2
Aaron Koller <aaronkoller777@gmail.com>
2017-08-26 23:03:51 -0700

    Refactored using KeyValuePairResource

--- Mapping/MappingProfile.cs
+++ Mapping/MappingProfile.cs
@@ -12,14 +12,15 @@ namespace Vega.Mapping
         {
             // Domain to API Resource
             CreateMap<Make, MakeResource>();
-            CreateMap<Model, ModelResource>();
-            CreateMap<Feature, FeatureResource>();
+            CreateMap<Make, KeyValuePairResource>();
+            CreateMap<Model, KeyValuePairResource>();
+            CreateMap<Feature, KeyValuePairResource>();
             CreateMap<Vehicle, SaveVehicleResource>()
               .ForMember(vr => vr.Contact, opt => opt.MapFrom(v => new ContactResource { Name = v.ContactName, Email = v.ContactEmail, Phone = v.ContactPhone } ))
               .ForMember(vr => vr.Features, opt => opt.MapFrom(v => v.Features.Select(vf => vf.FeatureId)));
             CreateMap<Vehicle, VehicleResource>()
               .ForMember(vr => vr.Contact, opt => opt.MapFrom(v => new ContactResource { Name = v.ContactName, Email = v.ContactEmail, Phone = v.ContactPhone } ))
-              .ForMember(vr => vr.Features, opt => opt.MapFrom(v => v.Features.Select(vf => new FeatureResource{Id = vf.Feature.Id, Name = vf.Feature.Name })))
+              .ForMember(vr => vr.Features, opt => opt.MapFrom(v => v.Features.Select(vf => new KeyValuePairResource{Id = vf.Feature.Id, Name = vf.Feature.Name })))
               .ForMember(vr => vr.Make, opt => opt.MapFrom(v => v.Model.Make));
 
             // API Resource to Domain
