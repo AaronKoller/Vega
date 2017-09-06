49d71c3df325ab7a591dca2d77bd1dcc677fc560
Aaron Koller <aaronkoller777@gmail.com>
2017-09-05 23:09:05 -0700

    ngZones

--- ClientApp/app/components/app/app.error-handler.ts
+++ ClientApp/app/components/app/app.error-handler.ts
@@ -1,17 +1,21 @@
 import { ToastyService } from 'ng2-toasty';
-import { ErrorHandler, Inject } from "@angular/core";
+import { ErrorHandler, Inject, NgZone } from "@angular/core";
 
 export class AppErrorHandler implements ErrorHandler {
-    constructor(@Inject(ToastyService) private ToastyService: ToastyService) {      
+    constructor(
+        private ngZone: NgZone,
+        @Inject(ToastyService) private ToastyService: ToastyService) {      
     }
 
     handleError(error: any): void {
-      this.ToastyService.error({
-        title: 'Error',
-        msg: 'An unexpected error happened.',
-        theme: 'bootstrap',
-        showClose: true,
-        timeout: 5000
-      })
+        this.ngZone.run(() => {
+            this.ToastyService.error({
+                title: 'Error',
+                msg: 'An unexpected error happened.',
+                theme: 'bootstrap',
+                showClose: true,
+                timeout: 5000
+              })
+        })
     }
 }
